25111252ada37989c3fa3141030a8c174fbdc123
Franklin Chen <myfreedom614@gmail.com>
2020-05-21 15:12:44 +0800

    Clear useless using namespace

--- FuncAppV3/TimeoutHttpTriggerTest.cs
+++ FuncAppV3/TimeoutHttpTriggerTest.cs
@@ -1,13 +1,9 @@
-using System;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using PlayFab.Samples;
-using PlayFab;
  
 namespace PlayFab.Samples
 {
